39e82d74243fd3003abd551742d10dcc35ba3980
web-coder-pro <oralboyf@gmail.com>
2025-05-14 11:20:47 +0100

    Create 1-lesson.js
    1-lesson for JS

---
+++ 1-lesson.html/1-lesson.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>1-dars</title>
+</head>
+<body>
+<script>
+	console.log("Abdulvahhob");
+
+	console.log("VebTest saytini tayyorlash va uni hosting qilish");
+	console.log("VebTest saytidan ko'p trafik olish va undan pul topish.")
+
+	console.log("Men bu yildan boshlab katta dasturchi bo'laman.")
+</script>
+</body>
+</html>
