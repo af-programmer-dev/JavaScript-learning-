9f2d8a7669f759b24a10d59ab7faa993ad54920d
web-coder-pro <oralboyf@gmail.com>
2025-05-14 17:18:10 +0100

    Create 3-lesson.js
    3-lesson for JS

---
+++ 1-lesson.html/3-lesson.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>3-dars</title>
+</head>
+<body>
+
+
+<script>
+	
+	let ism = "Abdulvahhob";
+	let yosh = 14;
+	let tel = true;
+	let jins = null;
+	let none = undefined;
+
+	console.log(ism);
+	console.log(yosh);
+	console.log(tel);
+	console.log(jins);
+	console.log(none);
+
+	// objekt - murakkab malumotlar
+
+	let foydalanuchi = {
+		name: "Abdulvahhob",
+		old: 14,
+		online: true,
+	};
+
+	console.log(foydalanuchi.name);
+	console.log(foydalanuchi.old);
+	console.log(foydalanuchi.online);
+
+	// Array - ro'yxat(massiv)
+	// Birnecha qiymatni bir joyda saqlash mumkin.
+
+	let baholar = [5, 4, 3, 5, 3];
+	console.log(baholar[0]); // 5
+	console.log(baholar.lenght); // 4
+
+	// Typof operatori.
+	// Bu operator orqali o'zgaruvchilarni turini bilib olsa bo'ladi.
+	console.log(typeof name);
+	console.log(typeof ism);
+	console.log(typeof yosh);
+
+
+</script>
+
+
+
+</body>
+</html>
