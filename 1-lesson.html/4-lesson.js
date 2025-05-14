2575536f59f01153547cd3e7818dbf4616663562
web-coder-pro <oralboyf@gmail.com>
2025-05-14 17:19:20 +0100

    Create 4-lesson.js
    4-lesson for JS

---
+++ 1-lesson.html/4-lesson.js
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>4-dars</title>
+</head>
+<body>
+
+	<script>
+		
+		// 1 - topshiriq!
+		let a = 7;
+		let b = 3;
+
+		console.log(a + b);
+		console.log(a - b);
+		console.log(a * b);
+		console.log(a / b);
+		console.log(a % b);
+		console.log(a ** b);
+
+		// 2 - topshiriq!
+
+		let x = 10;
+
+		x += 1; 
+		x *= 2;
+		x -= 3;
+		x /= 4;
+
+		console.log(x);
+
+		// 3 - topshiriq!
+
+		let y = 8;
+		let z = "8";
+
+		console.log(y == z);
+		console.log(y === z);
+		console.log(y != z);
+		console.log(y !== z);
+
+		// 4 - topshiriq!
+
+		let age = 14;
+		let isActive = false;
+
+		console.log(age >= 14 && isActive);
+		console.log(age < 14 || isActive);
+		console.log(! isActive);
+
+	</script>
+
+
+</body>
+</html>
