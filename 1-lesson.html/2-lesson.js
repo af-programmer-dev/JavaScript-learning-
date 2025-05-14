a87eafb344f20dd2c679f6503b9d807267d341e8
web-coder-pro <oralboyf@gmail.com>
2025-05-14 11:31:39 +0100

    Create 2-lesson.js
    2-lesson for JS

---
+++ 1-lesson.html/2-lesson.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>2-dars</title>
+</head>
+<body>
+<script type="text/javascript">
+	// Topshiriqlar
+
+	let ism = "Abdulvahhob";
+	let yosh = 14;
+	let maqsad = "AFWA - akademiyasini yaratish";
+	let kun = "3-kun";
+
+	console.log("Salom, mening ismim: " + ism);
+	console.log("Mening yoshim: " + yosh);
+	console.log("Mening maqsadim: " + maqsad);
+	console.log("Bugungi darsning kuni: " + kun);
+
+	let name = "Ali";
+	console.log("Mening ismim", name);
+</script>
+</body>
+</html>
